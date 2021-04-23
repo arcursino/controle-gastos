581f3f3646fcd3ad84e1eedf36ebaac5451f4ded
arcursino <38140925+arcursino@users.noreply.github.com>
2021-04-23 19:25:54 -0300

    Delete keys.js

--- backend/keys.js
+++
@@ -1,4 +0,0 @@
-//é preciso definir o <usuario>, <senha> e <BD>. O BD deverá ser bdatv
-module.exports = {
-    mongoUrl: "mongodb+srv://ariana:jyLA8nZCJJcmyWH@cluster0-ig4eb.mongodb.net/bdatv?retryWrites=true&w=majority"
-}
